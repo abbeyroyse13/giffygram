4c1ef8ef3ebab88f29f8c875631e638dc0b664e7
abbeyroyse13 <heatherruan126@gmail.com>
2021-03-10 15:13:49 -0600

    added event listeners

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,4 @@
-import { getPosts } from "./data/DataManager.js"
+import { getPosts, usePostCollection } from "./data/DataManager.js"
 import { PostList } from "./feed/PostList.js"
 import { NavBar } from "./nav/NavBar.js"
 import { Footer } from "./nav/Footer.js"
@@ -11,6 +11,64 @@ import { Footer } from "./nav/Footer.js"
 //let postElement = document.querySelector(".postList");
 //let navElement = document.querySelector("nav");
 //let entryElement = document.querySelector(".entryForm")
+
+const applicationElement = document.querySelector(".giffygram");
+
+applicationElement.addEventListener("click", event => {
+	if (event.target.id === "logout"){
+		console.log("You clicked on logout")
+	}
+})
+
+applicationElement.addEventListener("click", event => {
+	if (event.target.id === "directMessageIcon"){
+		console.log("You clicked on the fountain pen")
+	}
+})
+
+applicationElement.addEventListener("click", event => {
+	if (event.target.id === "pbjar"){
+		console.log("You're going home")
+	}
+})
+
+applicationElement.addEventListener("click", (event) => {
+	
+	if (event.target.id.startsWith("edit")){
+		console.log("post clicked", event.target.id.split("--"))
+		console.log("the id is", event.target.id.split("--")[1])
+	}
+})
+
+applicationElement.addEventListener("change", event => {
+    if (event.target.id === "yearSelection") {
+      const yearAsNumber = parseInt(event.target.value)
+  
+      console.log(`User wants to see posts since ${yearAsNumber}`)
+    }
+  })
+
+applicationElement.addEventListener("change", event => {
+  if (event.target.id === "yearSelection") {
+    const yearAsNumber = parseInt(event.target.value)
+    console.log(`User wants to see posts since ${yearAsNumber}`)
+    //invoke a filter function passing the year as an argument
+    showFilteredPosts(yearAsNumber);
+  }
+})
+
+const showFilteredPosts = (year) => {
+  //get a copy of the post collection
+  const epoch = Date.parse(`01/01/${year}`);
+  //filter the data
+  const filteredData = usePostCollection().filter(singlePost => {
+    if (singlePost.timestamp >= epoch) {
+      return singlePost
+    }
+  })
+  const postElement = document.querySelector(".postList");
+  postElement.innerHTML = PostList(filteredData);
+}
 
 const showPostList = () => {
     const postElement = document.querySelector(".postList");
