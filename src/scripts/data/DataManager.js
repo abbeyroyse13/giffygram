d5c0dbb1f983d892a43b627b0f60c1e2cb97de65
abbeyroyse13 <heatherruan126@gmail.com>
2021-03-23 11:56:19 -0500

    added delete button

--- src/scripts/data/DataManager.js
+++ src/scripts/data/DataManager.js
@@ -1,51 +1,63 @@
 export const getUsers = () => {
 
     return fetch("http://localhost:8088/users")
-    .then(response => response.json())
+        .then(response => response.json())
 
-    }
+}
 
-    let postCollection = [];
+let postCollection = [];
 
-    export const usePostCollection = () => {
-      //Best practice: we don't want to alter the original state, so
-      //make a copy of it and then return it
-      //The spread operator makes this quick work
-      return [...postCollection];
-    }
-    export const getPostCollection = () => {
-      return fetch("http://localhost:8088/posts")
+export const usePostCollection = () => {
+    //Best practice: we don't want to alter the original state, so
+    //make a copy of it and then return it
+    //The spread operator makes this quick work
+    return [...postCollection];
+}
+export const getPostCollection = () => {
+    return fetch("http://localhost:8088/posts")
         .then(response => response.json())
         .then(parsedResponse => {
-          postCollection = parsedResponse
-          return parsedResponse;
+            postCollection = parsedResponse
+            return parsedResponse;
         })
-    }
+}
 
-    export const getPosts = () => {
-        
-        return fetch("http://localhost:8088/posts")
-    .then(response => response.json())
-    }
+export const getPosts = () => {
 
-    export const createPost = postObj => {
-      return fetch("http://localhost:8088/posts", {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json"
-          },
-          body: JSON.stringify(postObj)
-    
-      })
-          .then(response => response.json())
-    }
+    return fetch("http://localhost:8088/posts")
+        .then(response => response.json())
+}
 
-    const loggedInUser = {
-        id: 1,
-        name: "Bryan",
-        email: "bryan@bn.com"
-    }
-    
-    export const getLoggedInUser = () => {
-        return {...loggedInUser};
-    }
+export const createPost = postObj => {
+    return fetch("http://localhost:8088/posts", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(postObj)
+
+        })
+        .then(response => response.json())
+}
+
+export const deletePost = postId => {
+    return fetch(`http://localhost:8088/posts/${postId}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+
+        })
+        .then(response => response.json())
+        .then(getPosts)
+}
+
+const loggedInUser = {
+    id: 1,
+    name: "Bryan",
+    email: "bryan@bn.com"
+}
+
+export const getLoggedInUser = () => {
+    return {...loggedInUser };
+}
